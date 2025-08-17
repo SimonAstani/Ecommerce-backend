8140bc97e897f23e24fb5532dce8b0ef6f169b82
sumanastani <sumanastani@gmail.com>
2025-08-17 19:30:04 -0400

    implemented users, roles and address table logic

--- src/main/java/com/eCommerce/EcommerceBackend/userPreference/model/Address.java
+++ src/main/java/com/eCommerce/EcommerceBackend/userPreference/model/Address.java
@@ -1,14 +1,64 @@
 package com.eCommerce.EcommerceBackend.userPreference.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "address")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
 
+    @NotBlank
+    @Size(min = 5, message = "Street name must be atleast 5 characters")
+    private String street;
+
+
+    @NotBlank
+    @Size(min = 5, message = "Bulidng name must be atleast 5 character")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 4, message = "City name must be atleast 4 character")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message = "state name must be atleast 2 character")
+    private String state;
+
+    @NotBlank
+    @Size(min = 2, message = "country name must be atleast 2 character")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, message = "Zipcode name must be atleast 6 character")
+    private String zipcode;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+    public Address(String street, String buildingName, String city, String state, String country, String zipcode) {
+        this.street = street;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.zipcode = zipcode;
+    }
 }
+
+
--- src/main/java/com/eCommerce/EcommerceBackend/userPreference/model/User.java
+++ src/main/java/com/eCommerce/EcommerceBackend/userPreference/model/User.java
@@ -1,16 +1,16 @@
 package com.eCommerce.EcommerceBackend.userPreference.model;
 
+import com.eCommerce.EcommerceBackend.model.Product;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -52,5 +52,16 @@ public class User {
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    @Getter
+    @Setter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_address", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "address_id"))
+    private List<Address> addresses = new ArrayList<>();
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+                orphanRemoval = true)
+    private Set<Product> products;
 }
 
